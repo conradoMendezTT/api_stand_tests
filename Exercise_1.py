9ca8836b4f72867943e196decb0f225914d6b413
conrado.mendez <conrado.mendez@tripleten-team.com>
2025-01-04 23:34:59 -0300

    first exercise === OK

--- Exercise_1.py
+++ Exercise_1.py
@@ -3,3 +3,21 @@
 
 # - Define la clase `Persona` con un constructor que acepte `nombre` y `edad`.
 # - Implementa un método `saludar` que imprima "Hola, mi nombre es [nombre] y tengo [edad] años."
+
+
+class Persona:
+    def __init__(self,nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        print(self.nombre)
+
+
+    def saludar(self):
+        print(f"Hola mi nombre es {self.nombre} y tengo {self.edad} años")
+
+
+# Implementacion
+
+persona_1 = Persona("Conrado" , 25)
+
+persona_1.saludar()
