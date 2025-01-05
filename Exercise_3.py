580f131d3678b270b9a849781094ff395ae2eefe
conrado.mendez <conrado.mendez@tripleten-team.com>
2025-01-05 00:15:04 -0300

    first try - Exercise 3 === OnWork

--- Exercise_3.py
+++ Exercise_3.py
@@ -7,3 +7,28 @@
 # - Define la clase `Curso` con un constructor que acepte `nombre` y una lista vacía de `alumnos`.
 # - Implementa el método `agregar_alumno` que añada un objeto `Alumno` a la lista `alumnos`.
 # - Implementa un método `listar_alumnos` que imprima los nombres de todos los alumnos en el curso.
+
+
+class Alumno:
+    def __init__(self, nombre , id):
+        self.nombre = nombre
+        self.id = id
+
+class Curso:
+    def __init__(self, nombre_curso):
+        self.nombre_curso = nombre_curso
+        self.alumnos = []
+
+
+    def agregar_alumnos(self , alumno):
+        self.alumnos.append(alumno)
+
+    def listar_alumnos(self):
+        print(self.alumnos)
+
+
+alumno_1 = Alumno("Conrado" , 1 )
+curso_1 = Curso("Primer Curso creado")
+
+curso_1_info = curso_1.agregar_alumnos(alumno_1)
+print(f"Los alumnos agregados son {curso_1_info}")
