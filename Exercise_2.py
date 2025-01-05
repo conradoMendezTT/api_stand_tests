696fc11fd4319c89c630d227fd0b2613f334265d
conrado.mendez <conrado.mendez@tripleten-team.com>
2025-01-04 23:55:41 -0300

    exercise 2 === DONE

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,3 +6,22 @@
 # - Define la clase `Motor` con un constructor que acepte `potencia`.
 # - Define la clase `Coche` con un constructor que acepte una instancia de `Motor`.
 # - Implementa un método `arrancar` en `Coche` que use la potencia del motor para imprimir "El coche ha arrancado con una potencia de [potencia] caballos."
+
+
+class Coche:
+    def __init__(self, potencia):
+        self.motor = Motor(potencia)
+
+
+    def arrancar(self):
+       # potencia = self.motor
+        print(f"El coche ha arrancado con una potencia de {self.motor.potencia} caballos.")
+
+class Motor:
+    def __init__(self, potencia):
+        self.potencia = potencia
+
+
+coche_1 = Coche(1500)
+coche_1.arrancar()
+
